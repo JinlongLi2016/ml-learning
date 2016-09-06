12a522b410c27695c8931a8b30d8ba4aeef89802
Computer-Room <longtolin@foxmail.com>
2016-09-06 16:16:39 +0800

    Some Formation of Codes.

--- ex4/nnCostFunction.m
+++ ex4/nnCostFunction.m
@@ -62,8 +62,6 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
-%%%LJL: before 
-
 a1 = [ones(1,m); X'];   % Add a row of ZERO
 a2 = sigmoid(Theta1 * a1);
 a2 = [ones(1,m); a2];  % a2 26*m
@@ -71,14 +69,6 @@ a3 = (sigmoid(Theta2 * a2));%%; % a3 m*10  prediction of i^th sample(i belong to
 h_Theta = a3;  % h_Theta :The output layer's output
 a3 = [ones(1,m); a3];
 
-
-%ix1_y = y; ix2_y = 1:m;
-%y = zeros(num_labels, m);
-%y(ix1_y, ix2_y) = 1;
-%y = y';
-%fprintf('sum(y)=%d\n',sum(y));
-%fprintf('sum(h_Theta)=%f\n',sum(h_Theta));
-%fprintf("sum( (y .* log(h_Theta)' + (1-y) .* log(1-h_Theta)' )(:) )/(-m)=%f\n",...
 ix = y;
 y = zeros(num_labels, m);
 
@@ -87,19 +77,12 @@ for cac=1:m
 end
 y = y';
 
-
-
 J = sum( (y .* log(h_Theta)' + (1-y) .* log(1-h_Theta)' )(:) )/(-m)...
 +( sum( (Theta1(:,2:end) .* Theta1(:, 2:end) )(:)) + sum(( Theta2(:,2:end).*Theta2(:,2:end) )(:)) )* lambda/(2*m);
 
-
-
 delta3 = (h_Theta - y'); % 10*m
 delta2 = (Theta2' * delta3) .* (a2 .* (1-a2));  % 26*m
-% delta2 = delta2(2:end, :); % 25*m
-
-delta1 = ( Theta1' * delta2(2:end, :) ) .* ( a1 .* (1 - a1) );%401*m
-
+% No need to cac delta1, cause no error in layer 1(input layer)
 
 DELTA1 = delta2(2:end, :)*(a1)'; %25*401
 DELTA2 = delta3*a2';% 10*26
@@ -109,23 +92,6 @@ Theta1_grad(:,2:end) = DELTA1(:,2:end)/m + lambda*Theta1(:,2:end)/m;
 
 Theta2_grad(:,1) = DELTA2(:, 1)/m;
 Theta2_grad(:,2:end) = DELTA2(:, 2:end)/m + lambda*Theta2(:, 2:end)/m;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
